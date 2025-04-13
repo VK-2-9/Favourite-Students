affc385f4dc052a73c8071b304b8b3ad8130c3d9
VK-2-9 <vishalnath002@gmail.com>
2025-04-13 12:25:57 +0530

    coml

--- src/components.js/ListContext.jsx
+++ src/components.js/ListContext.jsx
@@ -5,41 +5,7 @@ const ListContext=createContext()
 function ListContextProvider(data){
 
     const [studentList,setStudentList]=useState([
-        {
-            id:1,
-            name:"Messi",
-            clicked:false,
-        },
-        {
-            id:2,
-            name:"Ronaldo",
-            clicked:false,
-        },
-        {
-            id:3,
-            name:"Neymar",
-            clicked:false,
-        },
-        {
-            id:4,
-            name:"Gavi",
-            clicked:false,
-        },
-        {
-            id:5,
-            name:"Pedri",
-            clicked:false,
-        },
-        {
-            id:6,
-            name:"Yamal",
-            clicked:false,
-        },
-        {
-            id:7,
-            name:"Ramos",
-            clicked:false,
-        },
+        
     ])
    
     const[favStudentList,setFavStudentList]=useState([])
